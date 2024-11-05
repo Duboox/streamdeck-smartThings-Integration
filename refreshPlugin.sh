030e97954bbdd1517ce975a07a01431aca9ee7d4
Joshua Martinez <joshdevjs@gmail.com>
2024-11-05 19:49:22 -0400

    better refresh script

--- refreshPlugin.sh
+++ refreshPlugin.sh
@@ -1,23 +1,40 @@
 #!/bin/sh
 
-echo 'Killing the Stream Deck process'
-
-if [[ "$OSTYPE" == "msys" ]]; then
-  taskkill //IM "StreamDeck.exe" //F
-else
-  pkill 'Stream Deck'
-fi
-
+#Define Variables
+streamdeckBrand='ajazz' # elgato || ajazz
 pluginName='com.duboox.streamdeck.smartthings'
-
-if [[ "$OSTYPE" == "msys" ]]; then
-  pluginsDir="$HOME\AppData\Roaming\Elgato\StreamDeck\Plugins"
-else
-  pluginsDir="$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins"
-fi
 projectDir=$(PWD)
+typeOsBrand="$OSTYPE-$streamdeckBrand"
 
-echo 'Building from sources'
+
+# Define OS-Brands for kill deck process
+declare -a killProcess
+killProcess[msys-elgato]="taskkill /IM \"StreamDeck.exe\" /F" # Windows El Gato
+killProcess[darwin24-elgato]="pkill 'Stream Deck'"  # MAC El Gato
+killProcess[msys-ajazz]="taskkill /IM \"StreamDockAJAZZ.exe\" /F" # Windows Ajazz
+killProcess[darwin24-ajazz]="pkill 'Stream Dock AJAZZ'" # MAC Ajazz
+
+# Define OS-Brands for pluginsDir
+declare -a pluginsDirs
+pluginsDirs[msys-elgato]="$HOME\AppData\Roaming\Elgato\StreamDeck\Plugins" # Windows El Gato
+pluginsDirs[darwin24-elgato]="$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins"  # MAC El Gato
+pluginsDirs[msys-ajazz]="$HOME\AppData\Roaming\HotSpot\StreamDeck\plugins" # Windows Ajazz
+pluginsDirs[darwin24-ajazz]="$HOME/Library/Application Support/HotSpot/StreamDock/plugins" # MAC Ajazz
+
+# Define OS-Brands for Re-Open deck process
+declare -a openProcess
+openProcess[msys-elgato]="'C:\Program Files\Elgato\StreamDeck\StreamDeck.exe' &" # Windows El Gato
+openProcess[darwin24-elgato]="open /Applications/Elgato\ Stream\ Deck.app &"  # MAC El Gato
+openProcess[msys-ajazz]="'C:\Program Files\Hotspot\StreamDeck\StreamDeck Ajazz.exe' &" # Windows Ajazz
+openProcess[darwin24-ajazz]="open /Applications/Stream\ Dock\ AJAZZ.app &" # MAC Ajazz
+
+echo "Killing the Stream Deck process $typeOsBrand"
+eval "${killProcess[$typeOsBrand]}"
+
+# Define PluginsDir
+pluginsDir="${pluginsDirs[$typeOsBrand]}"
+
+# echo 'Building from sources'
 npm run build
 
 echo "Installing the $pluginName plugin to $pluginsDir"
@@ -43,9 +60,4 @@ popd
 echo "Done installing ${pluginName}"
 
 # Reopen the Stream Deck app on background
-if [[ "$OSTYPE" == "msys" ]]; then
-  "C:\Program Files\Elgato\StreamDeck\StreamDeck.exe" &
-else
-  open /Applications/Elgato\ Stream\ Deck.app &
-fi
-exit
+eval "${openProcess[$typeOsBrand]}"
